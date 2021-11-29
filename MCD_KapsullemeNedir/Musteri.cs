e7643501ce292f8cfbe0a0b47c39c37e81dc4b83
emre gemici <gmcc.emree@gmail.com>
2021-11-29 17:29:49 +0300

    Add project files.

---
+++ MCD_KapsullemeNedir/Musteri.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_KapsullemeNedir
+{
+    class Musteri
+    {
+        //Class => Field (Alan)
+        int id;
+        public string isim;
+        public string soyisim;
+
+        //Class => Property
+        private string emailAdres;
+        public string EmailAdres
+        {
+            //get; //Database veya farklı bir veri kaynağı class çağıran programcıya datayı gösterdiğimiz alan
+            
+            //set; // Dış dünyadan alınan datanın içerisinde private olarak saklanan field içerisine değer atandığı kısım...
+
+            set
+            {
+                this.emailAdres = value;
+            }
+            get
+            {
+                return this.emailAdres;
+            }
+
+
+        }
+
+        
+
+    }
+}
---
+++ MCD_KapsullemeNedir/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_KapsullemeNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri M1 = new Musteri();
+            /*
+             * Field değer ataması yapılmasın ama değer okunabilsin.
+             * Field değer ataması yapılsın ama değer okunamasın.
+             * Field değer ataması yapılsın ama değerin ilk 4 hanesi okunsun.
+             * Field değer ataması ve okuma işlemleri yapılsın ama bu iki işlemde bizim belirlediğimiz bir süreçten geçsin veri daha sonra gösterilsin.
+             * 
+             */
+           // M1.emailAdres = "gmcc.emree@gmail.com";  (field)
+
+            // Genel olarak kapsülleme konusuna giriş yaptık.
+            M1.EmailAdres = "gmcc.emree@gmail.com";
+            Console.WriteLine(M1 .EmailAdres );
+
+            Console.ReadKey();
+
+        }
+    }
+}
